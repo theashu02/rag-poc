89a6ffd3fffe7436000f1907fc2c3955c84849a7
theashu02 <ashutoshchauhan1919@gmail.com>
2025-08-18 15:27:50 +0530

    add sidebar but not not fixed

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,13 +1,15 @@
 import { ChatInterface } from "./components/chatUI/ChatInterface";
 import HealthCheck from "./components/common/HealthCheck";
 import NetworkHealthBar from "./components/common/NetworkStatus";
+import { Sidebar } from "./components/common/Sidebar";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center mx-auto h-screen bg-blue-500">
+    <div className="flex h-screen">
+      {/* <Sidebar /> */}
       <NetworkHealthBar />
       <HealthCheck />
-      <div className="flex flex-col items-center gap-4 w-screen bg-red-300">
+      <div className="flex flex-col items-center w-full bg-red-300">
         <ChatInterface />
       </div>
     </div>
