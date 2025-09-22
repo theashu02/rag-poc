9098f9f45b3e3b9cc1899c8ee9a0e3c3d7b2aa2a
theashu02 <ashutoshchauhan1919@gmail.com>
2025-09-23 01:11:06 +0530

    add some fixes and add the react-intersection observer

--- apps/frontend/app/documents/page.tsx
+++ apps/frontend/app/documents/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { UploadedFilesList } from "../components/common/UploadFileList";
 import { Sidebar } from "../components/common/Sidebar";
 import { UploadedFilesList } from "../components/common/FileUpload/UploadedFilesList";
 
